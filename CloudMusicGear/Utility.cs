db6398d3a239bc2aff2b1e04396ecabeb6f2b4f0
bin456789 <bin456789@gmail.com>
2016-03-24 17:08:20 +0800

    work again, using api v2

--- CloudMusicGear/Utility.cs
+++ CloudMusicGear/Utility.cs
@@ -28,5 +28,38 @@ namespace CloudMusicGear
                 return "";
             }
         }
+
+        /// <summary>
+        /// Require the content of a URL via POST method.
+        /// </summary>
+        /// <param name="url">URL to post.</param>
+        /// <param name="data">data to post</param>
+        /// <returns></returns>
+        public static string Post(string url, string data)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers.Add("Cookie", "os=pc");
+
+                var bytes = Encoding.UTF8.GetBytes(data);
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(bytes, 0, bytes.Length);
+                }
+
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                return new StreamReader(response.GetResponseStream(), Encoding.UTF8).ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(url);
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
     }
 }
